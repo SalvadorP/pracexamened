e9cefadc0e77ceeaeb379a75f7a0bb43ca4eb2ca
Salvador P <salvadorperezd@gmail.com>
2024-03-09 23:00:57 +0100

    Commit inicial
    Agregado el proyecto y empezada la clase persona

---
+++ PracExamen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaExamenED
+{
+    public class Persona
+    {
+        public string nombre;
+        public int altura;
+        public double peso;
+        public int edad;
+
+        public void Persona() {
+            this.nombre = '';
+            this.altura = '';
+            this.peso = 0.0;
+            this.edad = 0;
+        }
+
+        public double imc() {
+            double imc = 0.0;
+            imc = peso / ()
+
+        }
+    }
+}
