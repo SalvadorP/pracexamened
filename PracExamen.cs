3bcec39ef123f456c8efdac3fade949463c6ad48
Salvador P <salvadorperezd@gmail.com>
2024-03-10 00:34:44 +0100

    [CAMBIO] Agregado codigo para la clase PracExamen.

--- PracExamen.cs
+++ PracExamen.cs
@@ -13,17 +13,107 @@ namespace PracticaExamenED
         public double peso;
         public int edad;
 
-        public void Persona() {
-            this.nombre = '';
-            this.altura = '';
+        public const int ERROR_ESTATURA = -1;
+        public const int ERROR_PESO = -1;
+        public const int ERROR_NOMBRE = 0;
+        public const int ERROR_EDAD = -1;
+
+        public Persona() {
+            this.nombre = "";
+            this.altura = 0;
             this.peso = 0.0;
             this.edad = 0;
         }
 
+        public Persona(string nombre,int altura,double peso,int edad) {
+            Nombre = nombre;
+            this.Altura = altura;
+            Peso = peso;
+            this.Edad = edad;
+        }
+
+        public string Nombre {
+            get => nombre;
+            set {
+                if (value != "") {
+                    this.nombre = value;
+                } else {
+                    this.nombre = ERROR_NOMBRE.ToString();
+                }
+            }
+        }
+
+        public int Altura {
+            get => altura;
+            set {
+
+                if (value > 1) {
+                    this.altura = value;
+                } else {
+                    this.altura = ERROR_ESTATURA;
+                }
+            }
+        }
+
+        public double Peso {
+            get => peso;
+            set {
+                if (value > 1) {
+                    this.peso = value;
+                } else {
+                    this.peso = ERROR_PESO;
+                }
+            }
+        }
+
+        public int Edad {
+            get => edad;
+            set {
+                if (value > 0) {
+                    this.edad = value;
+                } else {
+                    this.edad = ERROR_EDAD;
+                }
+            }
+        }
+
         public double imc() {
             double imc = 0.0;
-            imc = peso / ()
+            if (peso < 1) {
+                return Persona.ERROR_PESO;
+            }
+            if (altura < 10) {
+                return Persona.ERROR_ESTATURA;
+            }
 
+            imc = (peso / ((altura/100) * (altura/100)) );
+
+            return imc;
+        }
+
+        public string estaturaMedia() {
+            string estatura = "";
+            if (altura > 0) {
+                if (altura < 100) {
+                    estatura = "Posible Acondroplasia";
+                }
+                if (altura > 100 && altura < 180) {
+                    estatura = "normal";
+                }
+                if (altura > 180) {
+                    estatura = "mayor de la media";
+                }
+            } else {
+                estatura = Persona.ERROR_ESTATURA.ToString();
+            }
+            return estatura;
+        }
+
+        public string mostrarPersona() {
+            return this.nombre + "\n" +
+            this.altura + " cms\n" +
+            this.peso + " kgs\n" +
+            this.edad + " anyos\n";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,8 +10,26 @@ namespace PracticaExamenED
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Practica Examen ED");
+            Persona person = new Persona();
+            person.Nombre = "Darrow";
+            Console.WriteLine(person.mostrarPersona());
+            Console.WriteLine("IMC = " + person.imc() + "; Estatura media => " + person.estaturaMedia());
 
-            Console.ReadKey();
+            Persona person2 = new Persona("Sebro", 150, 60, 25);
+            Console.WriteLine(person2.mostrarPersona());
+            Console.WriteLine("IMC = " + person2.imc() + "; Estatura media => " + person2.estaturaMedia());
+
+            Persona person3 = new Persona("Ragnar", 210, 130, -26);
+            Console.WriteLine(person3.mostrarPersona());
+            Console.WriteLine("IMC = " + person3.imc() + "; Estatura media => " + person3.estaturaMedia());
+
+            Persona person4 = new Persona("Mustang", 200, 0, 23);
+            Console.WriteLine(person4.mostrarPersona());
+            Console.WriteLine("IMC = " + person4.imc() + "; Estatura media => " + person4.estaturaMedia());
+
+
+
         }
     }
 }
